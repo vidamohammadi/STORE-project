2311da8e51c6dddf0993ab4b5d9737acb6f253a7
vida <m.vidamohammadi@gmail.com>
2021-12-09 13:43:40 +0330

    project structure-v1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React , { Component } from 'react';
-import './App.css';
+import Wrapper from './hoc/Wrapper'
+import Layout from './components/Layout/Layout'
+import Shopping from './containers/Shopping/Shopping';
 
 class App extends Component{
   render(){
     return(
-      <div>App</div>
+      <Wrapper>
+        <Layout>
+          <Shopping />
+        </Layout>
+      </Wrapper>
     )
   }
 }
